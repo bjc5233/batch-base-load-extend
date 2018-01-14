fc2746e7caa63834df73a87c9a02e5400cf6707f
bjc5233 <bjc5233@gmail.com>
2018-01-15 04:46:13 +0800

    add common funs

--- loadFuncs/_ifAnd.bat
+++ loadFuncs/_ifAnd.bat
@@ -11,25 +11,22 @@
 ::      call loadF.bat _ifAnd
 ::      set a=1& set b=1& set c=1
 ::      (set and1=%a%==1& set and2=%b%==1& set and3=%c%==2& call %_ifAnd% flag 3)
-::      (%flag%) && (echo true) || (echo false)
+::      if defined flag (echo true) else (echo false)
 ::  ------ demo.bat代码 ------
 ::  输出结果=====> true
 
-set "_true=echo.>nul"
-set "_false=set=2>nul"
-if "%1"=="" (set "%1=%_false%"& goto :EOF)
+if "%1"=="" goto :EOF
 if "%2"=="" (set conditionCount=2) else (set conditionCount=%2)
+set %1=
 set _ifAndTemp=%temp%\_ifAnd%conditionCount%.bat
 if not exist "%_ifAndTemp%" (
     (
         echo @echo off
-        echo set "_true=%_true%"
-        echo set "_false=%_false%"
         echo set _if_and_flag=0
-        for /l %%i in (1,1,%conditionCount%) do (echo if not defined and%%i ^(set "%%1=%%_false%%"^& goto :EOF^))
+        for /l %%i in (1,1,%conditionCount%) do (echo if not defined and%%i goto :EOF)
         for /l %%i in (1,1,%conditionCount%) do (set /p"=if %%and%%i%% "<nul)
         echo set _if_and_flag=1
-        echo ^(if %%_if_and_flag%%==1 ^(set "%%1=%%_true%%"^) else ^(set "%%1=%%_false%%"^)^)^& goto :EOF
+        echo ^(if %%_if_and_flag%%==1 set "%%1=1"^)^& goto :EOF
     )>%_ifAndTemp%
 )
 call %_ifAndTemp% %1
